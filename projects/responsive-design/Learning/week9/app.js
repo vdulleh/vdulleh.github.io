fa4abe73a949b7e27e27efb2434aedd393de8989
Varmah Layee DULLEH <dullehvarmah@gmail.com>
2026-03-23 19:05:22 +1100

    updated information on arthemictal

--- projects/responsive-design/Learning/week9/app.js
+++ projects/responsive-design/Learning/week9/app.js
@@ -1,9 +1,20 @@
-const firstNumber = 200;
-const secondNumber = 100;
+const firstNumber = 55;
+const secondNumber = "55";
 
-const sum = firstNumber + secondNumber;
+let sum = firstNumber + secondNumber;
 const difference = firstNumber - secondNumber;
 const product = firstNumber * secondNumber;
 const quotient = firstNumber / secondNumber;
 const remainder = firstNumber % secondNumber;
 const exponentitation = firstNumber **2;
+
+
+const complexExpression = (firstNumber + secondNumber) * 2 - 10 / 5;
+ 
+
+
+sum += 10;
+
+//Arthmetic operators
+// comparision operators
+// logical operators (OR, AND)
